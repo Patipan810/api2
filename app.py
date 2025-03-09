99f4782a03ae392ef7b48b4777692617862dd476
Patipan810 <120148751+Patipan810@users.noreply.github.com>
2025-03-09 18:59:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://itbit0267.cpkkuhost.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
